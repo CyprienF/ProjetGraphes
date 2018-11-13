08e399f1e7c0fe34bf8f81a8e5fb43395c9fa064
CyprienF <floquet.cyprien@gmail.com>
2018-11-13 22:34:43 +0100

    Add all markers on the map + event listener

--- src/main/java/vue/GrapheFXMLController.java
+++ src/main/java/vue/GrapheFXMLController.java
@@ -2,9 +2,13 @@ package vue;
 
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import modele.Carrefour;
+import modele.ListeCarrefours;
+import netscape.javascript.JSObject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,69 +18,78 @@ public class GrapheFXMLController implements Initializable, MapComponentInitiali
     @FXML
     protected GoogleMapView mapView;
 
-    private GoogleMap map;
+    private ListeCarrefours listeCarrefours;
+
+    private Carrefour selectedCarrefour1;
+
+    private Carrefour selectedCarrefour2;
+
+    private int numberOfSelectedCarrefours;
 
     public void initialize(URL url, ResourceBundle rb) {
         mapView.addMapInializedListener(this);
     }
 
-    public void mapInitialized() {
-        LatLong joeSmithLocation = new LatLong(47.6197, -122.3231);
-        LatLong joshAndersonLocation = new LatLong(47.6297, -122.3431);
-        LatLong bobUnderwoodLocation = new LatLong(47.6397, -122.3031);
-        LatLong tomChoiceLocation = new LatLong(47.6497, -122.3325);
-        LatLong fredWilkieLocation = new LatLong(47.6597, -122.3357);
+    private void initializeAllCarrefours() {
+        this.listeCarrefours = new ListeCarrefours();
+        this.listeCarrefours.initialisationListeCarrefours();
+    }
 
-        //Set the initial properties of the map.
+    public void mapInitialized() {
+        this.initializeAllCarrefours();
+
+        LatLong carrefourLocation;
+
+        Marker markerCarrefour;
+
+        MarkerOptions markerOptionsCarrefour;
+
+        LatLong lyonLocation = new LatLong(45.764043, 4.835658999999964);
+
+        // Options de la carte
         MapOptions mapOptions = new MapOptions();
 
-        mapOptions.center(new LatLong(47.6097, -122.3331))
+        mapOptions.center(lyonLocation)
                 .mapType(MapTypeIdEnum.ROADMAP)
                 .overviewMapControl(false)
                 .panControl(false)
                 .rotateControl(false)
                 .scaleControl(false)
                 .streetViewControl(false)
-                .zoomControl(false)
-                .zoom(12);
+                .zoomControl(true)
+                .zoom(16);
 
-        map = mapView.createMap(mapOptions);
+        GoogleMap map = mapView.createMap(mapOptions);
 
-        //Add markers to the map
-        MarkerOptions markerOptions1 = new MarkerOptions();
-        markerOptions1.position(joeSmithLocation);
+        for(Carrefour carrefour : this.listeCarrefours.getMesCarrefours()) {
 
-        MarkerOptions markerOptions2 = new MarkerOptions();
-        markerOptions2.position(joshAndersonLocation);
+            // Position du carrefour
+            carrefourLocation = new LatLong(carrefour.getCoordY(), carrefour.getCoordX());
 
-        MarkerOptions markerOptions3 = new MarkerOptions();
-        markerOptions3.position(bobUnderwoodLocation);
+            // Ajout du marker sur la carte
+            markerOptionsCarrefour = new MarkerOptions();
+            markerOptionsCarrefour.position(carrefourLocation);
 
-        MarkerOptions markerOptions4 = new MarkerOptions();
-        markerOptions4.position(tomChoiceLocation);
+            markerCarrefour = new Marker(markerOptionsCarrefour);
 
-        MarkerOptions markerOptions5 = new MarkerOptions();
-        markerOptions5.position(fredWilkieLocation);
+            map.addMarker(markerCarrefour);
 
-        Marker joeSmithMarker = new Marker(markerOptions1);
-        Marker joshAndersonMarker = new Marker(markerOptions2);
-        Marker bobUnderwoodMarker = new Marker(markerOptions3);
-        Marker tomChoiceMarker= new Marker(markerOptions4);
-        Marker fredWilkieMarker = new Marker(markerOptions5);
+            map.addUIEventHandler(markerCarrefour, UIEventType.click, (JSObject obj) -> {
 
-        map.addMarker( joeSmithMarker );
-        map.addMarker( joshAndersonMarker );
-        map.addMarker( bobUnderwoodMarker );
-        map.addMarker( tomChoiceMarker );
-        map.addMarker( fredWilkieMarker );
+                if(numberOfSelectedCarrefours == 0) {
+                    selectedCarrefour1 = carrefour;
+                }
 
-        InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
-        infoWindowOptions.content("<h2>Fred Wilkie</h2>"
-                + "Current Location: Safeway<br>"
-                + "ETA: 45 minutes" );
+                if(numberOfSelectedCarrefours == 1) {
+                    selectedCarrefour2 = carrefour;
+                }
 
-        InfoWindow fredWilkeInfoWindow = new InfoWindow(infoWindowOptions);
-        fredWilkeInfoWindow.open(map, fredWilkieMarker);
+                numberOfSelectedCarrefours++;
+
+                System.out.println(selectedCarrefour1);
+                System.out.println(selectedCarrefour2);
+            });
+        }
     }
 
 }
